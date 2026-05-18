bb2fa492e9b4f05d2678df553f3dd29f02b43a5f
Nickname4th <2497957861@qq.com>
2026-05-18 18:30:42 +0800

    fix(ios): handle Alipay universal link callbacks

--- ios/AppLifecycleDelegate.swift
+++ ios/AppLifecycleDelegate.swift
@@ -11,34 +11,42 @@ import AlipaySDK
 
 public class AppLifecycleDelegate: ExpoAppDelegateSubscriber {
     public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return handleOpenURL(url)
+    }
+    
+    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return handleOpenURL(url)
+    }
+
+    public func application(_ application: UIApplication,
+                            continue userActivity: NSUserActivity,
+                            restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        return AFServiceCenter.handleOpenUniversalLink(userActivity) { result in
+            self.emitPayResult(result)
+        }
+    }
+
+    private func handleOpenURL(_ url: URL) -> Bool {
         if url.host == "safepay" {
             AlipaySDK.defaultService().processOrder(withPaymentResult: url) { result in
-                ExpoAlipayModule.moduleInstance?.sendEvent("onPayResult", (result as? [String: Any]) ?? [:])
+                self.emitPayResult(result)
             }
             return true
         }
         if url.host == "oauth" {
             AlipaySDK.defaultService().processAuth_V2Result(url) { result in
-                ExpoAlipayModule.moduleInstance?.sendEvent("onAuthResult", (result as? [String: Any]) ?? [:])
+                self.emitAuthResult(result)
             }
             return true
         }
         return false
     }
-    
-    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if url.host == "safepay" {
-            AlipaySDK.defaultService().processOrder(withPaymentResult: url) { result in
-                ExpoAlipayModule.moduleInstance?.sendEvent("onPayResult", (result as? [String: Any]) ?? [:])
-            }
-            return true
-        } 
-        if url.host == "oauth" {
-            AlipaySDK.defaultService().processAuth_V2Result(url) { result in
-                ExpoAlipayModule.moduleInstance?.sendEvent("onAuthResult", (result as? [String: Any]) ?? [:])
-            }
-            return true
-        }
-        return false
+
+    private func emitPayResult(_ result: Any?) {
+        ExpoAlipayModule.moduleInstance?.sendEvent("onPayResult", (result as? [String: Any]) ?? [:])
+    }
+
+    private func emitAuthResult(_ result: Any?) {
+        ExpoAlipayModule.moduleInstance?.sendEvent("onAuthResult", (result as? [String: Any]) ?? [:])
     }
 }
